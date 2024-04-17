5d0bde55312f75ac2ba06d301e577ac289becce0
AdrianS100304 <jose13699631@gmail.com>
2024-04-17 17:57:06 -0600

    Página web para concumir api de Digimons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DigimonesServices } from './digimones/digimones.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'digimon';
+  title = 'digimon JASS';
+
+  digimones: any;
+
+  constructor(public digimon: DigimonesServices) {
+
+  }
+
+  ngOnInit() {
+    this.digimon.getDigimones().subscribe
+      (
+        (r) => {
+          this.digimones = r; console.log(r)
+        },
+        (e) => { console.log(e) }
+      )
+  }
 }
